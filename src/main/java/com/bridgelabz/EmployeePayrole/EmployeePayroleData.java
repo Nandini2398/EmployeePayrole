741ea1ca6c149374b11122cc07677dca2eef87c0
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-25 08:01:41 +0530

    ADDED:initial commit

---
+++ src/main/java/com/bridgelabz/EmployeePayrole/EmployeePayroleData.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.EmployeePayrole;
+
+public class EmployeePayroleData {
+		int employeeId;
+		String employeeName;
+		double employeeSalary;
+		public EmployeePayroleData(Integer id, String name, Double salary) {
+			this.employeeId = id;
+			this.employeeName = name;
+			this.employeeSalary = salary;
+		}
+		@Override
+		public String toString() {
+			return "Employee Details: \nEmployee Id: "+employeeId+"\nEmployee Name: "+employeeName+"\nEmployee Salary: "+employeeSalary;
+		}
+	}
